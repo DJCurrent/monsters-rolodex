753db48bffbfa8223526053b05ad083bae5d1cf9
Cessil <cessil@Cessils-iMac.local>
2020-01-30 23:24:20 +0900

    Updates

---
+++ precache-manifest.1e62fe372c269e88f883ccb63bea955e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "834f3406ef11ef070d2980f71298bc9a",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "4dac74f52cb3dec61537",
+    "url": "/monsters-rolodex/static/css/main.1882bdf2.chunk.css"
+  },
+  {
+    "revision": "c68e14fdc7e1ba4a5621",
+    "url": "/monsters-rolodex/static/js/2.9e7d0785.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/monsters-rolodex/static/js/2.9e7d0785.chunk.js.LICENSE"
+  },
+  {
+    "revision": "4dac74f52cb3dec61537",
+    "url": "/monsters-rolodex/static/js/main.a2f3effa.chunk.js"
+  },
+  {
+    "revision": "9e014af7b12dc10ef867",
+    "url": "/monsters-rolodex/static/js/runtime-main.ff038b7c.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/monsters-rolodex/static/media/logo.5d5d9eef.svg"
+  }
+]);
